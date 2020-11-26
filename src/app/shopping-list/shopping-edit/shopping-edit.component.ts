0cc7d667900b52c625832af59a722affafe6cbb6
Anshul Dahiya <anshul.rts@gmail.com>
2020-11-26 15:46:02 +0530

    Fetching values

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
@@ -11,10 +11,11 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
   constructor(private slService: ShoppingListService) { }
+  @ViewChild('f') slform: NgForm;
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
-
+  editedItem: Ingredient;
   
   ngOnInit(): void {
     this.subscription = this.slService.startedEditing
@@ -22,6 +23,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         (index: number) => {
           this.editMode = false;
           this.editedItemIndex = index;
+          this.editedItem = this.slService.getIngredient(this.editedItemIndex);
+          this.slform.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          });
         }
     )
   }
